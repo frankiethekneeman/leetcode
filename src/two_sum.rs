35c5c544236b939d5712667109f5274e471b685e
Francis Sirizzotti <francis@sirizzotti.info>
2024-07-23 11:28:54 -0400

    More idiomatic types

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
  *  I did not get to choose the signature here.  I believe it should return (usize, usize)...
  *  or even Option<(usize, usize)>.
  */
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Option<(usize, usize)>{
     /*
      *  Per the Constraints of the problem, there will only ever be exactly one solution.
      *  incidentally, this means that either there will only be exactly _two_ of a number, or
@@ -17,17 +17,10 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let (idx, first) = nums
         .into_iter()
         .enumerate()
-        .find(|(i, x)| lookup.get(&(target - x)).map_or(false, |j| j != i))
-        /*
-         *  Blech.  But the constraint is there _will_ be a solution, so the return
-         *  type does not allow for Monads
-         */
-        .unwrap();
+        .find(|(i, x)| lookup.get(&(target - x)).map_or(false, |j| j != i))?;
 
-    [idx, lookup[&(target - first)]]
-        .into_iter()
-        .map(|i| i as i32) // Don't love this.
-        .collect()
+    lookup.get(&(target - first))
+        .map(|i| (idx, *i))
 }
 
 // I cannot believe this doesn't exist already
@@ -39,23 +32,23 @@ fn swap<L, R>((l, r): (L, R)) -> (R, L) {
 mod test_two_sum {
     use super::*;
 
-    fn case(nums: Vec<i32>, target: i32, expected: Vec<i32>) {
+    fn case(nums: Vec<i32>, target: i32, expected: Option<(usize, usize)>) {
         let actual = two_sum(nums, target);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_case_1() {
-        case(vec![2, 7, 11, 15], 9, vec![0, 1])
+        case(vec![2, 7, 11, 15], 9, Some((0, 1)));
     }
 
     #[test]
     fn test_case_2() {
-        case(vec![3, 2, 4], 6, vec![1, 2])
+        case(vec![3, 2, 4], 6, Some((1, 2)))
     }
 
     #[test]
     fn test_case_3() {
-        case(vec![3, 3], 6, vec![0, 1])
+        case(vec![3, 3], 6, Some((0, 1)))
     }
 }
